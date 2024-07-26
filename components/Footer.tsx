4ba3aab57caad0e8741c3198b482f774bc678cc2
Lucas <lucasaraujo1964@gmail.com>
2024-07-25 22:10:51 -0300

    feat: adding footer

---
+++ components/Footer.tsx
@@ -0,0 +1,61 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { footerLinks } from "@/constants";
+
+const Footer = () => {
+  return (
+    <footer className="flex flex-col text-black-100 mt-5 border-t border-gray-100">
+      <div className="flex max-md:flex-col flex-wrap justify-between gap-5 sm:px-16 px-6 py-10">
+        <div className="flex flex-col justify-start items-start gap-6">
+          <div className="flex items-center gap-1">
+            <Image
+              src="/logo.png"
+              alt="Car Hub Logo"
+              width={40}
+              height={10}
+              className="object-contain"
+            />
+            <h1 className="font-bold text-xl">
+              Wheel<span className="text-primary-purple">Zone</span>
+            </h1>
+          </div>
+          <p className="text-base text-gray-700">
+            WheelZone 2024 All rights reserved &copy;
+          </p>
+        </div>
+
+        <div className="footer__links">
+          {footerLinks.map((link) => (
+            <div key={link.title} className="footer__link">
+              <h3 className="font-bold">{link.title}</h3>
+              {link.links.map((item) => (
+                <Link
+                  key={item.title}
+                  href={item.url}
+                  className="text-gray-500"
+                >
+                  {item.title}
+                </Link>
+              ))}
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex justify-between items-center flex-wrap mt-10 border-t border-gray-100 sm:px-16 px-6 py-10">
+        <p>@2024 WheelZone. All Rights Reserved</p>
+        <div className="footer__copyrights-link">
+          <Link href="/" className="text-gray-500">
+            Privacy Policy
+          </Link>
+          <Link href="/" className="text-gray-500">
+            Terms of Use
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
